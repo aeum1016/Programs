846bc6568000ebd8c8d0a3a8bc5c3eb02d6ddad3
aeum1016 <aeum1016@gmail.com>
2022-07-13 22:52:31 -0400

    CP4: Done with all adhoc problems. I think I singlehandedly made the difficulty level of windows go up .2 with the number of incorrect submissions I gave. But found a UCF professor with the correct solution (dijkstras) and used it to solve the problem. Not too mad about not recognizing dijkstras, but wayyyy too many other mistakes mostly from not reading the problem

--- src/UFPTPractice/problem_types/adhoc/windows.cpp
+++ src/UFPTPractice/problem_types/adhoc/windows.cpp
@@ -4,21 +4,24 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
 
 class Box
 {
   public:
-    Box(int x, int y, int w, int h)
+    int x, y;
+    int w, h;
+    int index;
+    Box(int x, int y, int w, int h, int c)
     {
       this->x = x;
       this->y = y;
       this->w = w;
       this->h = h;
+      this->index = c;
     }
-    int x, y;
-    int w, h;
     void move(int xDelta, int yDelta)
     {
       x+=xDelta;
@@ -31,15 +34,17 @@ class Box
     }
     bool overlaps(int nX, int nY, int nW, int nH)
     {
-      if(nX > x && nY > y && nX < x+w && nY < y+h) return true;
-      if(nX > x && nY+nH > y && nX < x+w && nY+nH < y+h) return true;
-      if(nX+nW > x && nY > y && nX+nW < x+w && nY+nH < y+h) return true;
-      if(nX+nW > x && nY+nH > y && nX+nW < x+w && nY+nH < y+h) return true;
-      return false;
+      Box b(nX, nY, nW, nH, -1);
+      if(this->x+this->w <= b.x) return false;
+      if(b.x+b.w <= this->x) return false;
+      if(this->y+this->h <= b.y) return false;
+      if(b.y+b.h <= this->y) return false;
+      return true;
     }
+
     bool contains(int nX, int nY)
     {
-      if(nX >= x && nY >= y && nX <= x+w && nY <= y+h) return true;
+      if(nX >= x && nY >= y && nX < x+w && nY < y+h) return true;
       return false;
     }
 };
@@ -54,87 +59,253 @@ class Window
     }
     int wX, wY;
     vector<Box*> boxes;
-    bool openBox(int x, int y, int w, int h)
+    int counter = 0;
+    int boxSize()
     {
-      if(x >= 0 && y >= 0 && x+w <= wX && y+h <= wY)
+      return boxes.size();
+    }
+
+    bool sortAsc(Box* a, Box* b)
+    {
+      return (a->x < b->x);
+    }
+
+    void printBoxes()
+    {
+      for(Box* b : boxes)
+      {
+        cout << b->x << " " << b->y << " " << b->w << " " << b->h << endl;
+      }
+    }
+
+    int openBox(int x, int y, int w, int h)
+    {
+      if(!outOfBounds(x, y, w, h))
       {
         bool overlap = false;
         for(Box* b : boxes)
         {
           if(b->overlaps(x, y, w, h)) overlap = true;
         }
-        if(overlap) return false;
+        if(overlap) return 1;
+
+        Box* b = new Box(x, y, w, h, counter);
+        counter++;
+        boxes.push_back(b);
+        return 0;
       }
-      else return false;
-      Box* b = new Box(x, y, w, h);
-      boxes.push_back(b);
-      return true;
+      else return 1;
     }
 
-    bool closeBox(int x, int y)
+    int closeBox(int x, int y)
     {
       for(Box* b : boxes)
       {
         if(b->contains(x, y))
         {
-          int index = find(boxes.begin(), boxes.end(), b)-boxes.begin();
           boxes.erase(find(boxes.begin(), boxes.end(), b));
-          return true;
+          return 0;
         }
       }
-      return false;
+      return 2;
     }
 
-    bool resizeBox(int x, int y, int w, int h)
+    int resizeBox(int x, int y, int w, int h)
     {
       for(Box* b : boxes)
       {
         if(b->contains(x, y))
         {
+          if(outOfBounds(b->x, b->y, w, h)) return 1;
           int index = find(boxes.begin(), boxes.end(), b)-boxes.begin();
           for(int i = 0; i < boxes.size(); i++)
           {
             if(i != index && (boxes.at(i)->overlaps(b->x, b->y, w, h)))
             {
-              return false;
+              return 1;
             }
           }
           b->resize(w, h);
-          return true;
+          return 0;
         }
       }
-      return false;
+      return 2;
     }
 
-    bool moveBox(int x, int y, int mX, int mY)
+    bool outOfBounds(int x, int y, int w, int h)
     {
+      return !(x >= 0 && y >= 0 && x+w <= wX && y+h <= wY);
+    }
+
+    vector<int> dX = vector<int>({1,0,-1,0});
+    vector<int> dY = vector<int>({0,1,0,-1});
+
+    int getDir(int dx, int dy)
+    {
+      int x = dx/abs(dx+dy);
+      int y = dy/(abs(dx+dy));
+      for(int i = 0; i < dX.size(); i++)
+      {
+        if(x == dX[i] && y == dY[i]) return i;
+      }
+      return -1;
+    }
+
+    int distToBorder(int dir, int bIndex)
+    {
+      if(dir == 0) return wX - (boxes.at(bIndex)->x + boxes.at(bIndex)->w);
+      else if(dir == 1) return wY - (boxes.at(bIndex)->y + boxes.at(bIndex)->h);
+      else if(dir == 2) return boxes.at(bIndex)->x;
+      else if(dir == 3) return boxes.at(bIndex)->y;
+      return -1;
+    }
+
+    int distBetween(int sIndex, int dIndex, int dir)
+    {
+      Box s = *boxes.at(sIndex);
+      Box d = *boxes.at(dIndex);
+      if(dir%2 == 0)
+      {
+        if(s.y >= d.y+d.h || s.y+s.h <= d.y) return -1;
+        if(dir == 0)
+        {
+          if(s.x + s.w > d.x) return -1;
+          else return d.x - (s.x + s.w);
+        }
+        if(s.x < d.x+d.w) return -1;
+        else return s.x - (d.x + d.w);
+      }
+      else
+      {
+        if(s.x >= d.x+d.w || s.x+s.w <= d.x) return -1;
+        if(dir == 1)
+        {
+          if(s.y+s.h > d.y) return -1;
+          else return d.y - (s.y+s.h);
+        }
+        if(s.y < d.y+d.h) return -1;
+        else return s.y - (d.y+d.h);
+      }
+      return -1;
+    }
+
+    int moveBox(int x, int y, int mX, int mY)
+    {
+      //dijkstras, following Arup Guha from UCF's solution
+      int maxMove = fmax(abs(mX), abs(mY));
+      int orgMove = maxMove;
+      int dir = getDir(mX, mY);
+
+      //store distances for Dijkstras
+      vector<int> dist(boxes.size(), -1);
+      vector<int> used(boxes.size(), false);
+      Box* first = nullptr;
       for(Box* b : boxes)
       {
         if(b->contains(x, y))
         {
-          int index = find(boxes.begin(), boxes.end(), b)-boxes.begin();
-          for(int i = 0; i < boxes.size(); i++)
-          {
-            if(i != index && (boxes.at(i)->overlaps(b->x+mX, b->y+mY, b->w, b->h)))
-            {
-              // moveBox(boxes.at(i)->x, boxes.at(i)->y, boxes.at(i).x - b->x+mX+(mX > 0 ? b->w : 0), boxes.at(i).y-(b->y+mY+(mY > 0 ? b->h: 0)));
-            }
-          }
-          b->move(w, h);
-          return true;
+          first = b;
+          break;
         }
       }
-      return false;
+      if(first == nullptr) return (1E9)+1;
+      int index = find(boxes.begin(), boxes.end(), first) - boxes.begin();
+      dist[index] = 0;
+
+      while(true)
+      {
+        //Find next best item
+        int best = -1, bestD = (1E9)+1;
+        for(int i = 0; i < boxes.size(); i++)
+        {
+          if(dist[i] != -1 && dist[i] < bestD && !used[i])
+          {
+            bestD = dist[i];
+            best=i;
+          }
+        }
+        if(best == -1 || bestD > maxMove) break;
+        used[best] = true;
+
+        maxMove = fmin(maxMove, distToBorder(dir, best) + dist[best]);
+
+        for(int i = 0; i < boxes.size(); i++)
+        {
+          if(i == best) continue;
+          
+          if(distBetween(best, i, dir) != -1)
+          {
+            if(dist[i] == -1 || dist[best] + distBetween(best, i, dir) < dist[i])
+            {
+              dist[i] = dist[best] + distBetween(best, i, dir);
+            }
+          }
+        }
+      }
+
+      for(int i = 0; i < boxes.size(); i++)
+      {
+        if(dist[i] != -1 && dist[i] < maxMove)
+        {
+          int toMove = maxMove - dist[i];
+          boxes[i]->move(dX[dir]*toMove, dY[dir]*toMove);
+        }
+      }
+      return maxMove;
     }
 };
 
 
 int main()
 {
-  Window w(320, 200);
-  cout << w.openBox(50, 50, 10, 10) << endl;
-  cout << w.openBox(70, 55, 10, 10) << endl;
-  cout << w.openBox(90, 55, 10, 10) << endl;
-  cout << w.resizeBox(55, 55, 20, 10) << endl;
-  cout << w.closeBox(110, 60) << endl;
+  int x, y;
+  scanf("%d %d/n", &x, &y);
+  Window w(x, y);
+  string command;
+  int cNum = 0;
+  while(getline(cin, command))
+  {
+    string commandP;
+    stringstream commandSS(command);
+    vector<string> com(5);
+    int i = 0;
+    while(getline(commandSS, commandP, ' '))
+    {
+      com[i] = commandP;
+      i++;
+    }
+    int result = 0;
+    if(com[0] == "OPEN")
+    {
+      result = w.openBox(stoi(com[1]), stoi(com[2]), stoi(com[3]), stoi(com[4]));
+    }
+    else if(com[0] == "CLOSE")
+    {
+      result = w.closeBox(stoi(com[1]), stoi(com[2]));
+    }
+    else if(com[0] == "RESIZE")
+    {
+      result = w.resizeBox(stoi(com[1]), stoi(com[2]), stoi(com[3]), stoi(com[4]));
+    }
+    else if(com[0] == "MOVE")
+    {
+      result = w.moveBox(stoi(com[1]), stoi(com[2]), stoi(com[3]), stoi(com[4]));
+    }
+    if(com[0] != "MOVE")
+    {
+      if(result == 1) cout << "Command " << cNum << ": " << com[0] << " - window does not fit" << endl;
+      else if(result == 2) cout << "Command " << cNum << ": " << com[0] << " - no window at given position" << endl;
+    }
+    else
+    {
+      if(abs(result) != fmax(abs(stoi(com[3])), abs(stoi(com[4]))))
+      {
+        if(result == (1E9)+1) cout << "Command " << cNum << ": " << com[0] << " - no window at given position" << endl;
+        else cout << "Command " << cNum << ": " << com[0] << " - moved " << abs(result) << " instead of " << abs(stoi(com[3]) ? stoi(com[3]) : stoi(com[4])) << endl;
+      }
+    }
+    cNum++;
+  }
+  cout << w.boxSize() << " window(s):" << endl;
+  w.printBoxes();
 }
